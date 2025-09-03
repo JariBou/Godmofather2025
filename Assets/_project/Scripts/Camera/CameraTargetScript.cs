db61d765ad82119821209fd66e464cfb3efe0a67
Jari Bou <bourdietome@gmail.com>
2025-09-03 11:06:09 +0200

    CameraShake

--- Assets/_project/Scripts/Camera/CameraTargetScript.cs
+++ Assets/_project/Scripts/Camera/CameraTargetScript.cs
@@ -1,26 +1,59 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _project.Scripts.Camera
 {
-    [RequireComponent(typeof(UnityEngine.Camera))]
+    [RequireComponent(typeof(UnityEngine.Camera), typeof(Animator))]
     public class CameraTargetScript : MonoBehaviour
     {
+        private static readonly int DoShake = Animator.StringToHash("DoShake");
+
         [SerializeField]
         private Vector2 _targetPosition;
+        
         private Vector3 TargetPositionV3 => new(_targetPosition.x, _targetPosition.y, transform.position.z);
+        private Vector3 _startPosition;
 
         [SerializeField]
         private AnimationCurve _moveCurve;
         
-        [SerializeField, Range(0f, 1f)]
+        [SerializeField, Range(0f, 5f)]
         private float _speed;
         
         [SerializeField]
-        private List<Transform> _testTargets = new();
-        private int _testIndex;
+        private List<Transform> _targets = new();
+        private int _targetIndex;
 
+        private int TargetIndex
+        {
+            get => _targetIndex;
+            set
+            {
+                _targetIndex = Mathf.Clamp(value, 0, _targets.Count-1);
+                _targetPosition = _targets[_targetIndex].position;
+                _startPosition = transform.position;
+                ResetTimer();
+            }
+        }
+
+        [ShowNonSerializedField]
         private float _internalTimer = 10f;
+
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            _targetPosition = _targets[TargetIndex].position;
+        }
 
         private void Update()
         {
@@ -28,23 +61,22 @@ namespace _project.Scripts.Camera
             {
                 _internalTimer += Time.deltaTime * _speed;
             }
-            transform.position = Vector3.Lerp(transform.position, TargetPositionV3, _moveCurve.Evaluate(_internalTimer));
+            transform.position = Vector3.Lerp(_startPosition, TargetPositionV3, _moveCurve.Evaluate(_internalTimer));
 
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                _testIndex = (_testIndex + 1) % _testTargets.Count;
-                _targetPosition = _testTargets[_testIndex].position;
-                ResetTimer();
+                if (_internalTimer >= (((TargetIndex + 1) % _targets.Count == 0) ? .7f : .2f))
+                {
+                    GotoNextTarget();
+                }
             } else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                _testIndex--;
-                if (_testIndex < 0)
+                if (_internalTimer >= (((TargetIndex - 1) < 0) ? .7f : .2f))
                 {
-                    _testIndex = _testTargets.Count - 1;
-                }
-                _targetPosition = _testTargets[_testIndex].position;
-                ResetTimer();
+                    GotoPreviousTarget();
+                }                
             }
+        
         }
 
         public void SetTargetPosition(Vector2 targetPosition)
@@ -56,6 +88,26 @@ namespace _project.Scripts.Camera
         private void ResetTimer()
         {
             _internalTimer = 0f;
+        }
+
+        public void GotoNextTarget()
+        {
+            if (TargetIndex == _targets.Count - 1)
+            {
+                _animator.SetTrigger(DoShake);
+                return;
+            }
+            ++TargetIndex;
+        }
+
+        public void GotoPreviousTarget()
+        {
+            if (TargetIndex == 0)
+            {
+                _animator.SetTrigger(DoShake);
+                return;
+            }
+            --TargetIndex;
         }
     }
 }
