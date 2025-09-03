fb3f040586b1d1564536a01ba6f183745b9bd5c2
Jari Bou <bourdietome@gmail.com>
2025-09-03 11:58:42 +0200

    Start of PhDetector

---
+++ Assets/_project/Scripts/Enigmas/PhEnigma/PhDetector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace _project.Scripts.Enigmas.PhEnigma
+{
+    public class PhDetector : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 _targetPosition;
+        private Vector3 _startPosition;
+
+        [SerializeField] 
+        private float _completionTime = 2f;
+
+        [SerializeField, Foldout("Display debug")] 
+        private bool _showTargetDisplay;
+        [SerializeField, Foldout("Display debug")]
+        private GameObject _targetDisplay;
+
+
+        private void Awake()
+        {
+            _startPosition = transform.position;
+            Destroy(_targetDisplay);
+        }
+
+        private void OnMouseDown()
+        {
+            transform.position = _targetPosition;
+            StartCoroutine(GoBack());
+        }
+
+        private IEnumerator GoBack()
+        {
+            yield return new WaitForSeconds(_completionTime);
+            transform.position = _startPosition;
+        }
+
+        #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            _targetDisplay.gameObject.SetActive(_showTargetDisplay);
+            _targetDisplay.transform.position = _targetPosition;
+        }
+        #endif
+    }
+}
