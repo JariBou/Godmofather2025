175f8a2ce8999a406cd8486eecdd7783563ab2d6
Jari Bou <bourdietome@gmail.com>
2025-09-05 12:01:35 +0200

    f

--- Assets/_project/Scripts/Menus/MainMenu.cs
+++ Assets/_project/Scripts/Menus/MainMenu.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string _playSceneName;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
 
     private void Update()
     {
