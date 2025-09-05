be245cb4d6e951142a5151cbb6dfaccc28abf933
GuillaumeRoPu <gllmrllndptz@gmail.com>
2025-09-05 10:59:43 +0200

    Changed Main Menu

--- Assets/_project/Scripts/Menus/CrazyEffects.cs
+++ Assets/_project/Scripts/Menus/CrazyEffects.cs
@@ -8,14 +8,14 @@ public class CrazyEffects : MonoBehaviour
     [SerializeField] private AnimationCurve _curve;
     [SerializeField] private TextMeshProUGUI _tmp;
     private float _timer;
-    [SerializeField] private float _colorChangeSpeed = 1f;
-    private float _hsvTest;
+    //[SerializeField] private float _colorChangeSpeed = 1f;
+    //private float _hsvTest;
     private bool _hasLooped = false;
 
     private void Start()
     {
         _timer = 0f;
-        _hsvTest = 0;
+        //_hsvTest = 0;
     }
 
     void Update()
@@ -40,10 +40,10 @@ public class CrazyEffects : MonoBehaviour
             _hasLooped = false;
         }
 
-        _tmp.color = Color.HSVToRGB((float)_hsvTest/360,1,1);
-        if (_hsvTest < 360)
-            _hsvTest += 1 * _colorChangeSpeed;
-        else
-            _hsvTest = 0;
+        //_tmp.color = Color.HSVToRGB((float)_hsvTest/360,1,1);
+        //if (_hsvTest < 360)
+        //    _hsvTest += 1 * _colorChangeSpeed;
+        //else
+        //    _hsvTest = 0;
     }
 }
