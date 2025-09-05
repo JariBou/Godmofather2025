b8dfdd60df41f9d3183554c2316c662aae20f05a
GuillaumeRoPu <gllmrllndptz@gmail.com>
2025-09-05 09:17:04 +0200

    Upgraded main menu

--- Assets/_project/Scripts/Menus/MainMenu.cs
+++ Assets/_project/Scripts/Menus/MainMenu.cs
@@ -1,17 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Image _img;
-
     [SerializeField] private string _playSceneName;
-
-    private void Start()
-    {
-        _img = GetComponent<Image>();
-    }
 
     private void Update()
     {
